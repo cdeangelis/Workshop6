e00001367f68a98dbc90aaed70128098f0751bfb
cdeangelis <cdeangelis@umass.edu>
2016-12-02 16:18:05 -0500

    validation

--- server/src/server.js
+++ server/src/server.js
@@ -6,11 +6,21 @@ var bodyParser = require('body-parser')
 // Imports the express Node module.
 var express = require('express');
 var readDocument = require('./database.js').readDocument
+var validate = require('express-jsonschema').validate;
+var StatusUpdateSchema = require('./schemas/statusupdate.json');
+var addDocument = require('./database.js').addDocument;
+var writeDocument = require('./database.js').writeDocument;
+
 
 
 // Creates an Express server.
 var app = express();
 
+
+// Support receiving text in HTTP request bodies
+app.use(bodyParser.text());
+// Support receiving JSON in HTTP request bodies
+app.use(bodyParser.json());
 
 // You run the server from `server`, so `../client/build` is `server/../client/build`.
 // '..' means "go up one directory", so this translates into `client/build`!
@@ -89,6 +99,101 @@ function getUserIdFromToken(authorizationLine) {
     return -1;
   }
 }
+
+
+/**
+* Get the feed data for a particular user.
+*/
+app.get('/user/:userid/feed', function(req, res) {
+  var userid = req.params.userid;
+  var fromUser = getUserIdFromToken(req.get('Authorization'));
+  // userid is a string. We need it to be a number.
+  // Parameters are always strings.
+  var useridNumber = parseInt(userid, 10);
+  if (fromUser === useridNumber) {
+    // Send response.
+    res.send(getFeedData(userid));
+  } else {
+    // 401: Unauthorized request.
+    res.status(401).end();
+  }
+});
+
+
+/**
+* Adds a new status update to the database.
+*/
+function postStatusUpdate(user, location, contents) {
+  // If we were implementing this for real on an actual server, we would check
+  // that the user ID is correct & matches the authenticated user. But since
+  // we're mocking it, we can be less strict.
+  // Get the current UNIX time.
+  var time = new Date().getTime();
+  // The new status update. The database will assign the ID for us.
+  var newStatusUpdate = {
+    "likeCounter": [],
+    "type": "statusUpdate",
+    "contents": {
+      "author": user,
+      "postDate": time,
+      "location": location,
+      "contents": contents,
+      "likeCounter": []
+    },
+    // List of comments on the post
+    "comments": []
+  };
+  // Add the status update to the database.
+  // Returns the status update w/ an ID assigned.
+  newStatusUpdate = addDocument('feedItems', newStatusUpdate);
+  // Add the status update reference to the front of the current user's feed.
+  var userData = readDocument('users', user);
+  var feedData = readDocument('feeds', userData.feed);
+  feedData.contents.unshift(newStatusUpdate._id);
+  // Update the feed object.
+  writeDocument('feeds', feedData);
+  // Return the newly-posted object.
+  return newStatusUpdate;
+}
+
+
+// `POST /feeditem { userId: user, location: location, contents: contents }`
+app.post('/feeditem',
+          validate({ body: StatusUpdateSchema }), function(req, res) {
+  // If this function runs, `req.body` passed JSON validation!
+  var body = req.body;
+  var fromUser = getUserIdFromToken(req.get('Authorization'));
+  // Check if requester is authorized to post this status update.
+  // (The requester must be the author of the update.)
+  if (fromUser === body.userId) {
+    var newUpdate = postStatusUpdate(body.userId, body.location,
+    body.contents);
+    // When POST creates a new resource, we should tell the client about it
+    // in the 'Location' header and use status code 201.
+    res.status(201);
+    res.set('Location', '/feeditem/' + newUpdate._id);
+    // Send the update!
+    res.send(newUpdate);
+  } else {
+    // 401: Unauthorized.
+    res.status(401).end();
+  }
+});
+
+
+/**
+* Translate JSON Schema Validation failures into error 400s.
+*/
+app.use(function(err, req, res, next) {
+  if (err.name === 'JsonSchemaValidation') {
+    // Set a bad request http response status
+    res.status(400).end();
+  } else {
+    // It's some other sort of error; pass it to next error middleware handler
+    next(err);
+  }
+});
+
 
 
 module.exports.getFeedData = getFeedData;
